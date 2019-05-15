bd3493626c89f26370ecb546fb78efad978b5d8e
mino89 <pierdomenicoreitano@gmail.com>
2019-05-15 23:25:08 +0200

    added wp theme

--- theme/index.php
+++ theme/index.php
@@ -1,40 +1,19 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-  <meta charset="<?php bloginfo( 'charset' ); ?>">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+<?php
 
-  <?php wp_head(); ?>
-</head>
+use Timber\Timber;
 
-<body <?php body_class(); ?>>
-
-<div class="wrap">
-  <div id="primary" class="content-area">
-    <main id="main" class="site-main" role="main">
-    MINO
-      <?php
-      if (have_posts()) :
-        /* Start the Loop */
-        while (have_posts()) : the_post();
-      ?>
-        <div>
-          <a href="<?php the_permalink(); ?>"><h3><?php the_title(); ?></h3></a>
-        </div>
-      <?php
-        endwhile;
-        /* End the Loop */
-      else :
-        // Nothing
-      endif;
-      ?>
-
-    </main><!-- #main -->
-  </div><!-- #primary -->
-</div><!-- .wrap -->
-
-<?php get_footer(); ?>
-<?php wp_footer(); ?>
-
-</body>
-</html>
+    global $paged;
+    if ( ! isset( $paged ) || ! $paged ) {
+        $paged = 1;
+    }
+    $context = Timber::get_context();
+    $args    = [
+        'post_type'      => 'post',
+        'posts_per_page' => 10,
+        'paged'          => $paged,
+    ];
+    $context['posts']      = new \Timber\PostQuery( $args );
+    $templates             = [
+        'pages/index.twig',
+    ];
+    Timber::render( $templates, $context );
---
+++ theme/page.php
@@ -0,0 +1,8 @@
+<?php
+
+
+use Timber\Timber;
+
+$context         = Timber::get_context();
+$context['post'] = Timber::query_post();
+Timber::render( 'pages/page.twig', $context );
